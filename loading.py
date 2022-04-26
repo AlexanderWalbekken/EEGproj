6b149ff84768c0655d5d38c43ff35eff221c67ce
annaekner <s193396@student.dtu.dk>
2022-04-26 20:34:51 +0200

    Included events in EpochsArray objects

--- loading.py
+++ loading.py
@@ -63,21 +63,63 @@ allEpochs = {}
 for subjectID in allFiles:
     subjectFile = loadmat(subjectID) # Subject file 
     
+    # Create MNE Info object 
     subjectTrials = subjectFile['data']['trial'][0,0].shape[1] # Number of trials
     subjectChannels = [elec[0] for elec in subjectFile['data']['label'][0,0][0,:]] # Number of electrodes
     subjectInfo = mne.create_info(subjectChannels, sfreq, ch_types = 'eeg') # MNE Info object 
 
     subjectData = [] 
+    subjectEventOnset = []
+    
+    numSamples = 550
+    onset = 0.58 #### This should probably be changed!! ####
     
     for i in range(subjectTrials):
-        subjectData.append(subjectFile['data']['trial'][0,0][0,i] * 10 **(-6))
-    
-    subjectData = np.array(subjectData) # Shape: (#trials, #channels, #timepoints)
-    subjectEpoch = mne.EpochsArray(subjectData, subjectInfo, tmin = -0.52) # MNE EpochsArray object
-    
-    allEpochs[subjectID[:-4]] = subjectEpoch # Add to dictionary
-    
+        subjectData.append(subjectFile['data']['trial'][0,0][0, i] * 10 **(-6))
         
+        # Events for MNE EpochsArray object 
+        trialSamples = subjectFile['data']['time'][0,0][0, i] 
+        trialEventOnset = len(trialSamples[trialSamples < onset])
+        subjectEventOnset.append(numSamples*i + trialEventOnset)
+    
+    
+    subjectEventOnset = np.array(subjectEventOnset).reshape((-1,1))
+    zeros = np.zeros((subjectTrials,1))
+    ids = subjectFile['trialtable'][:,0].reshape((-1,1))
+    
+    subjectEvents = np.hstack((subjectEventOnset, zeros, ids)).astype(int) # Shape: (#trials, 3)
+    subjectData = np.array(subjectData) # Shape: (#trials, #channels, #timepoints)
+    
+    subjectEpoch = mne.EpochsArray(subjectData, subjectInfo, events = subjectEvents, tmin = -0.52) # MNE EpochsArray object
+    allEpochs[subjectID[:-4]] = subjectEpoch # Add to allEpochs dictionary 
+    
+# ------------------------------ EVENT IDs --------------------------------- #
+
+eventIDs = { 'V_b_high' : 1, 
+             'V_b_mid'  : 2, 
+             'V_b_low'  : 3,
+             'V_g_high' : 4, 
+             'V_g_mid'  : 5, 
+             'V_g_low'  : 6,
+             'A_b_high' : 7,
+             'A_b_mid'  : 8, 
+             'A_b_low'  : 9,
+             'A_g_high' : 10,
+             'A_g_mid'  : 11, 
+             'A_g_low'  : 12,
+             'AV_b_high': 13, 
+             'AV_b_mid' : 14,
+             'AV_b_low' : 15,
+             'AV_g_high': 16, 
+             'AV_g_mid' : 17,
+             'AV_g_low' : 18,
+             'AV_TBD1_high' : 19,
+             'AV_TBD1_mid'  : 20,
+             'AV_TBD1_low'  : 21,
+             'AV_TBD2_high' : 19,
+             'AV_TBD2_mid'  : 20,
+             'AV_TBD2_low'  : 21  }
+
 # -------------------------------- TO DO ----------------------------------- #
 
 # TO-DO:
